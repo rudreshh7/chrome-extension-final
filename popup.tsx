e3e60a7f30051773e3104624eb9d208f45f42575
rudreshh7 <kumarrudresh5@gmail.com>
2023-08-20 10:19:46 +0530

    first commit

--- popup.tsx
+++ popup.tsx
@@ -1,27 +1,58 @@
-import { useState } from "react"
+import React, { useEffect, useState } from "react"
+
+import "./style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
+  const [locationData, setLocationData] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const fetchData = () => {
+    setIsLoading(true)
+    fetch("https://api.ipify.org?format=json")
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) => {
+        const ipAddress = data.ip
+
+        fetch(`https://ipinfo.io/${ipAddress}/json?token=INSERTHERE`)
+          .then((response) => {
+            return response.json()
+          })
+          .then((data) => {
+            setLocationData({
+              city: data.city,
+              country: data.country
+            })
+            setIsLoading(false)
+          })
+      })
+  }
+
+  const divStyle = {
+    width: "500px",
+    height: "500px"
+  }
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your
-        <a href="https://www.plasmo.com" target="_blank">
-          {" "}
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div style={divStyle} className="flex justify-center items-center h-screen">
+      <button
+        onClick={fetchData}
+        className="bg-red-500 text-white py-2 px-4 rounded">
+        Show My Location
+      </button>
+      {isLoading ? (
+        <p className="text-blue-700 text-base font-extrabold">Loading...</p>
+      ) : (
+        locationData && (
+          <div>
+            <h1 className="text-pink-500 text-lg font-bold">
+              Your country is {locationData.country} and city is{" "}
+              {locationData.city}
+            </h1>
+          </div>
+        )
+      )}
     </div>
   )
 }
